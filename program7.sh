e5ea5c482d18d2fecd57a43e61fd27cf98e93a4c
red <solarbeamred@gmail.com>
2026-06-09 16:30:24 +0530

    Create program7.sh

---
+++ program7.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update
+
+sudo apt install -y ansible
+
+mkdir -p ~/devops/program7
+
+cd ~/devops/program7
+
+###############################################################################
+# INVENTORY
+###############################################################################
+
+cat > hosts.ini << 'EOF'
+[local]
+localhost ansible_connection=local
+EOF
+
+###############################################################################
+# PLAYBOOK
+###############################################################################
+
+cat > setup.yml << 'EOF'
+---
+- name: Basic Server Setup
+  hosts: local
+  become: yes
+
+  tasks:
+
+    - name: Update apt cache
+      apt:
+        update_cache: yes
+
+    - name: Install curl
+      apt:
+        name: curl
+        state: present
+EOF
+
+###############################################################################
+# EXECUTE PLAYBOOK
+###############################################################################
+
+ansible-playbook -i hosts.ini setup.yml
