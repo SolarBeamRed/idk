1c574038b5e8830000a2d8418066076d64308838
red <solarbeamred@gmail.com>
2026-06-09 19:41:21 +0530

    Update program5.sh

--- program5.sh
+++ program5.sh
@@ -9,13 +9,12 @@ sudo apt install -y curl wget gnupg fontconfig openjdk-17-jre
 sudo mkdir -p /usr/share/keyrings
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key \
-| sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+| sudo gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.asc
 
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" \
 | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
-
 sudo apt install -y jenkins
 
 sudo systemctl enable jenkins
